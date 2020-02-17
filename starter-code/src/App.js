10d443bbeafe2608500684dd85e4cd5cafa8282b
Igor Zelenov <igorg.zelenov@gmail.com>
2020-02-17 17:01:15 -0500

    1-itteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom'
+import CountryDetails from './components/CountryDetails'
 import './App.css';
+import countries from './countries.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    countries// Same thing ===> countries:countries
+  }
+
+
+  showCountryLinks = () => {
+    let countryList = [...this.state.countries]
+    return countryList.map(eachCountry => {
+      return <Link key={eachCountry.cca3} to={`/country-detail/${eachCountry.cca3}`}>{eachCountry.name.common}</Link>
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <nav className="navbar navbar-dark bg-primary mb-3">
+            <div className="container">
+              <a className="navbar-brand" href="/">WikiCountries</a>
+            </div>
+          </nav>
+          <div className="container">
+            <div className="row">
+              <div className="col-5">
+                <div className="list-group">
+                  {this.showCountryLinks()}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/**Invisible */}
+        <Switch>
+          <Route exact path="/country-detail/:hippopotamus" render={(props) => <CountryDetails {...props} cool="super fantastically amaxzing" countries={this.state.countries} />} />
+
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
